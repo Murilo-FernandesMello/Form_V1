8ed8112ba3971a6af8af68c47d546d749b9fc96e
Murilo-FernandesMello <murilofmello07@gmail.com>
2022-10-28 21:05:58 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <div class="text-center mt-2">
+        <h1>Formulário</h1>
+    </div>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<div class="container">
+
+<div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label mt-5">Nome</label>
+    <div class="col-sm-10">
+      <input type="name" class="form-control border border-success mt-5" placeholder="nome">
+    </div>
+  </div>
+</div>
+
+<div class="container">
+<div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">E-mail</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control border border-success" placeholder="email.exemplo.com">
+    </div>
+  </div>
+
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Senha</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control border border-success" placeholder="senha" id="inputPassword">
+    </div>
+  </div>
+</div>
+
+<body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
